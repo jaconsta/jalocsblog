af43e85aec88768cdcc799b3e509188525deb28c
Javier Constain <javier@MainDeb.maindeb.com>
2014-07-08 00:17:41 -0500

    In hope admins problem is finished

--- jalocsBlog/urls.py
+++ jalocsBlog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'jalocsBlog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'post.views.showGeneralResume', name='home'),
-    url(r'^(?P<category>\w+)$', 'post.views.getCategoryResume', name='category_resume'),
-    url(r'^(?P<category>\w+)/(?P<post_id>\d+)/', 'post.views.getPostContent', name='post_content'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<category>\w+)/$', 'post.views.getCategoryResume', name='category_resume'),
+    url(r'^(?P<category>\w+)/(?P<post_id>\d+)/$', 'post.views.getPostContent', name='post_content'),
     url(r'^hello/$', TemplateView.as_view(template_name='main.html')),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
